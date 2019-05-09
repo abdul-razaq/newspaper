2920e654f8f4895b56d0962d74f76ffc9055c533
AbdulRazaq Suleiman <nivvy.nivans@gmail.com>
2019-05-09 02:45:14 +0100

    Initial commit

---
+++ users/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+# Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+
+    add_form = CustomUserCreationForm
+
+    form = CustomUserChangeForm
+
+    model = CustomUser
+
+    # Define what fields are to be displayed in the Admin site
+    list_display = ['email', 'username', 'age', 'is_staff',]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
